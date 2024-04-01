60398155c6fc7d4f18eae58b33997e23dd1ee2ce
hazemkhalid72 <129004905+hazemkhalid72@users.noreply.github.com>
2024-04-01 11:26:29 +0200

    6-problem in javaScript

---
+++ js/6-opposite-number.js
@@ -0,0 +1,16 @@
+// Very simple given an integer or a floating-pointing number find its oppsite.
+
+
+function oppsite(num){
+    console.log(`${num * -1}`)
+}
+
+oppsite(-90)
+
+   // // another solution 
+
+function oppsite(num){
+    console.log(`${-num}`)
+}
+
+oppsite(90)
